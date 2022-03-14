31a57a9d59ad2891b0835a0862c59b6eeabcaba8
AlexJob <henaochacon@gmail.com>
2022-03-14 14:59:39 -0500

    Orden

--- src/pages/details/index.js
+++ src/pages/details/index.js
@@ -4,9 +4,7 @@ import useGlobalGifs from "../../hooks/useGlobalGifs";
 
 export default function Detail({ params }) {
     const gifs = useGlobalGifs();
-    console.log(gifs);
 
     const gif = gifs.find(singleGif => singleGif.id === params.id);
-    console.log(gif);
     return <Gif {...gif} />
 }
